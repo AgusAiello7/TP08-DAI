cb5ba3c3a6f1bca4623d878d20051f805dbb2c4f
PEDRO MALOWICKI SANCHO <49552421@ort>
2026-05-18 12:14:20 -0300

    Inicializar proyecto Node. Crear Controller de provincias y configuración de DB.

---
+++ src/configs/dbconfig.js
@@ -0,0 +1,9 @@
+const config = {
+    host: "localhost",
+    database: "dai-events",
+    user: "postgres",
+    password: "root",
+    port: 4000
+}
+
+export default config;
---
+++ src/controllers/provinceController.js
@@ -0,0 +1,4 @@
+import {router} from 'express'; 
+import ProvinceService from './../services/province-service.js'
+const router = Router();
+const svc = new ProvinceService();
